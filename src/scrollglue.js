fd8ffa4dea64f94cd91ec22a23978a7ed6bc8c3d
Aviran <admin@avirancohen.com>
2013-09-25 23:22:38 +0300

    Browser Bug Fixed
    A small gap variable was inserted to avoid browser bug when manually
    scrolling to the bottom but not setting the 'shouldActivateAutoScroll()'
    to true;

--- src/scrollglue.js
+++ src/scrollglue.js
@@ -1,9 +1,9 @@
-(function(angular, undefined){
+(function (angular, undefined) {
     'use strict';
 
-    function fakeNgModel(initValue){
+    function fakeNgModel(initValue) {
         return {
-            $setViewValue: function(value){
+            $setViewValue: function (value) {
                 this.$viewValue = value;
             },
             $viewValue: initValue
@@ -11,30 +11,35 @@
     }
 
     angular.module('luegg.directives', [])
-    .directive('scrollGlue', function(){
+    .directive('scrollGlue', function () {
         return {
             priority: 1,
             require: ['?ngModel'],
             restrict: 'A',
-            link: function(scope, $el, attrs, ctrls){
+            link: function (scope, $el, attrs, ctrls) {
                 var el = $el[0],
                     ngModel = ctrls[0] || fakeNgModel(true);
 
-                function scrollToBottom(){
-                    el.scrollTop = el.scrollHeight;
+                function scrollToBottom() {
+					// Scroll value fixed, given scrollable value.
+                    el.scrollTop = el.scrollHeight - el.clientHeight;
                 }
 
-                function shouldActivateAutoScroll(){
-                    return el.scrollTop + el.clientHeight == el.scrollHeight;
+                function shouldActivateAutoScroll() {
+					// A small gap variable was inserted to avoid browser bug when manually scrolling to
+					// the bottom but not setting the 'shouldActivateAutoScroll()' to true;
+					var allowedGap = 5;
+					
+                    return el.scrollTop + el.clientHeight >= el.scrollHeight - allowedGap;
                 }
 
-                scope.$watch(function(){
-                    if(ngModel.$viewValue){
+                scope.$watch(function () {
+                    if (ngModel.$viewValue) {
                         scrollToBottom();
                     }
                 });
 
-                $el.bind('scroll', function(){
+                $el.bind('scroll', function () {
                     scope.$apply(ngModel.$setViewValue.bind(ngModel, shouldActivateAutoScroll()));
                 });
             }
